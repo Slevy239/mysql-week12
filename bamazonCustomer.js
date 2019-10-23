ba143900045105894a1094e0b9ee9a1b77081fe9
Sam Levy <slevy239@gmail.com>
2019-10-22 21:52:29 -0400

    display inquire

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,33 +11,59 @@ var connection = mysql.createConnection({
     insecureAuth: true
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw (err);
     console.log("Connected as id " + connection.threadId);
     afterConnection()
 });
 
 function afterConnection() {
-    connection.query("SELECT * FROM products" , function(err, res) {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw (err);
-        console.log(res);
+        // console.log(res);
         connection.end();
     })
 }
 
-// var display = function() {
-//     var query = "Select * from products";
-//     connection.query(query, function(err, res) {
-//         if (err) throw (err);
-//         var displayTable = new Table ({
-//             head: ["Item Id", "Product Name", "Category", "Price", "Qunatity"],
-//             colWidths: [10,25,25,10,14]
-//         });
-//         for (var i = 0; i < res.length; i++) {
-//             displayTable.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity])
-//         }
-//         console.log(displayTable.toString());
+var display = function () {
+    var query = "Select * from products";
+    connection.query(query, function (err, res) {
+        if (err) throw (err);
+        var displayTable = new Table({
+            head: ["Item Id", "Product Name", "Category", "Price", "Quantity"],
+            colWidths: [10, 25, 25, 10, 14]
+        });
+        for (var i = 0; i < res.length; i++) {
+            displayTable.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity])
+        }
+        console.log(displayTable.toString());
 
-//     })
-// }
-// display();
+    })
+}
+display();
+
+function productID() {
+    inquirer
+        .prompt([
+            {
+            name: "askID",
+            type: "input",
+            message: "What product ID would you like to purchase?",
+            filter: Number
+        }
+        ])
+        .then(function (answer) {
+            connection.query(
+                "INSERT INTO products SET ?",
+                {
+                    stock_quantity: answer.askID,
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log("Your auction was created successfully!");
+                    display();
+                }
+            )
+        })
+}
+productID();
