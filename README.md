a8b9b031b7197d33f1695acd44398433534da028
Sam Levy <slevy239@gmail.com>
2019-10-29 18:15:13 -0400

    readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mysql-week12
+
+![first screenshot](C:\Users\slevy\Documents\mysql-week12\screenshots/inital.png)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,8 +16,8 @@ connection.connect(function (err) {
     console.log("Connected as id " + connection.threadId);
 });
 function display() {
-    connection.query("SELECT * FROM products", function(err, res) {
-if (err) throw err;
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
         var table = new Table({
             head: ["ID", "ITEM NAME", "CATEGORY", "COST", "QUANTITY"],
             colWidths: [10, 15, 15, 10],
@@ -25,6 +25,7 @@ if (err) throw err;
             style: {
                 head: ["aqua"],
                 compact: true
+
             }
         });
         for (var i = 0; i < res.length; i++) {
@@ -35,18 +36,6 @@ if (err) throw err;
     })
 }
 
-
-// function afterConnection() {
-//     connection.query("SELECT * FROM products", function (err, res) {
-//         if (err) throw (err);
-//         console.log(" ID | ITEM NAME   | TYPE   | $     | QUANTITY")
-//         for (var i = 0; i < res.length; i++) {
-//             console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity)
-//         }
-//         console.log("---------------------------------------");
-//         question();
-//     });
-// }
 
 function question() {
     inquirer.prompt([
@@ -62,34 +51,38 @@ function question() {
             message: "How many would you like to purchase?",
             filter: Number
         }
+
     ])
         .then(function (answer) {
-            var id = answer.askID;
-            var query = connection.query(
-                "SELECT * From products WHERE item_id = ?", [id], function (err, res) {
-                    if (err) throw err;
-                    if (answer.quantity <= res[0].stock_quantity) {
-                        for (var i = 0; i < res.length; i++) {
-                            console.log("There are " + res[i].stock_quantity + " in stock!\n");
-                            console.log("You owe $" + res[i].price * answer.quantity);
-                        }
-                        var newQuantity = res[0].stock_quantity - answer.quantity;
-                        connection.query("UPDATE products SET stock_quantity = " + newQuantity + " WHERE item_id = " + res[0].item_id, function(err, resUpdate) {
-                            if (err) throw err;
-                            console.log("\nINVENTORY UPDATED");
-                            display();
+            if (answer.YorN === "YES") {
+                returnItem();
+            } else {
+
+                var id = answer.askID;
+                var query = connection.query(
+                    "SELECT * From products WHERE item_id = ?", [id], function (err, res) {
+                        if (err) throw err;
+                        if (answer.quantity <= res[0].stock_quantity) {
+                            for (var i = 0; i < res.length; i++) {
+                                console.log("There are " + res[i].stock_quantity + " in stock!\n");
+                                console.log("You owe $" + res[i].price * answer.quantity);
+                            }
+                            var newQuantity = res[0].stock_quantity - answer.quantity;
+                            connection.query("UPDATE products SET stock_quantity = " + newQuantity + " WHERE item_id = " + res[0].item_id, function (err, resUpdate) {
+                                if (err) throw err;
+                                console.log("\nINVENTORY UPDATED");
+                                display();
+                                connection.end();
+                            })
+                        } else {
+                            console.log("\nInsufficient quantity of " + res[0].stock_quantity + "!")
+                            console.log("We only have " + res[0].stock_quantity + " in stock!");
+                            console.log("Please try again!");
                             connection.end();
-                        })
-                    } else {
-                        console.log("\nInsufficient quantity of " + res[0].stock_quantity + "!")
-                        console.log("We only have " + res[0].stock_quantity + " in stock!");
-                        console.log("Please try again!");
-                        connection.end();
-                    }
-                })
+                        }
+                    })
+            }
         })
-
 }
-
 
 display();
