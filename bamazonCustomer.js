8dae91b16de6a97e37853d2309adc34df91f928d
Sam Levy <slevy239@gmail.com>
2019-10-30 20:40:24 -0400

    last prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,11 +23,11 @@ function display() {
             colWidths: [10, 15, 15, 10],
             colAligns: ["center", "left"],
             style: {
-                head: ["aqua"],
                 compact: true
 
             }
         });
+
         for (var i = 0; i < res.length; i++) {
             table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
         }
@@ -36,8 +36,8 @@ function display() {
     })
 }
 
-
 function question() {
+
     inquirer.prompt([
         {
             name: "askID",
@@ -54,35 +54,53 @@ function question() {
 
     ])
         .then(function (answer) {
-            if (answer.YorN === "YES") {
-                returnItem();
-            } else {
-
-                var id = answer.askID;
-                var query = connection.query(
-                    "SELECT * From products WHERE item_id = ?", [id], function (err, res) {
-                        if (err) throw err;
-                        if (answer.quantity <= res[0].stock_quantity) {
-                            for (var i = 0; i < res.length; i++) {
-                                console.log("There are " + res[i].stock_quantity + " in stock!\n");
-                                console.log("You owe $" + res[i].price * answer.quantity);
-                            }
-                            var newQuantity = res[0].stock_quantity - answer.quantity;
-                            connection.query("UPDATE products SET stock_quantity = " + newQuantity + " WHERE item_id = " + res[0].item_id, function (err, resUpdate) {
-                                if (err) throw err;
-                                console.log("\nINVENTORY UPDATED");
-                                display();
-                                connection.end();
-                            })
-                        } else {
-                            console.log("\nInsufficient quantity of " + res[0].stock_quantity + "!")
-                            console.log("We only have " + res[0].stock_quantity + " in stock!");
-                            console.log("Please try again!");
-                            connection.end();
+            var id = answer.askID;
+            var query = connection.query(
+                "SELECT * From products WHERE item_id = ?", [id], function (err, res) {
+                    if (err) throw err;
+                    if (answer.quantity <= res[0].stock_quantity) {
+                        for (var i = 0; i < res.length; i++) {
+                            console.log("\nThere are " + res[i].stock_quantity + " in stock!");
+                            console.log("You owe $" + res[i].price * answer.quantity);
                         }
-                    })
+                        var newQuantity = res[0].stock_quantity - answer.quantity;
+                        connection.query("UPDATE products SET stock_quantity = " + newQuantity + " WHERE item_id = " + res[0].item_id, function (err, resUpdate) {
+                            if (err) throw err;
+                            console.log("\nINVENTORY UPDATED");
+                            afterPurchase();
+
+                        })
+                    } else {
+                        console.log("\nInsufficient quantity of " + res[0].stock_quantity + "!")
+                        console.log("We only have " + res[0].stock_quantity + " in stock!");
+                        console.log("Please try again!");
+                        connection.end();
+                    }
+                })
+        })
+
+}
+
+
+
+function afterPurchase() {
+    inquirer.prompt([
+        {
+            name: "again",
+            type: "list",
+            message: "Would you like to make another purchase?",
+            choices: ["YES", "NO"]
+        }
+    ])
+        .then(function (answer) {
+            if (answer.again === "YES") {
+                display();
+            } else {
+                console.log("Thank you for shopping at BAMAZON!");
+                connection.end();
             }
         })
 }
+
 
 display();
